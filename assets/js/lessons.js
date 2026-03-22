aac7fabe939d5ec18f54c9c5eee8fdd16c5603be
tc <afoninartofficial@gmail.com>
2026-03-22 14:54:25 +0300

    upd (styles): Update styles on lesson page

--- assets/js/lessons.js
+++ assets/js/lessons.js
@@ -1,7 +1,34 @@
-const el = document.querySelector("#code__html");
+const elAppCode = document.querySelector("#code__app-block");
+const elHomeCode = document.querySelector("#code__home-block");
 
-const code = `
-<!doctype html>
+const blockAppCode = `#===== Сегодня можно не запоминать и не трогать =======
+# Добавление нужных инструментов из библиотеки
+from flask import Flask, render_template
+# Создание нашего приложения
+Flask app = Flask(__name__)
+#==========
+
+# ===== Наш основной блок, с которым работаем =======
+# табличка с адресом нужной страницы сайта
+@app.route("/")
+        def home():
+        return render_template("home.html", hp=75)
+
+# табличка с адресом нужной страницы сайта
+@app.route("/hello")
+        def hello():
+        return render_template("hello.html")
+#==========
+
+# ===== Сегодня можно не запоминать и не трогать =======
+# Если запускаем наше приложение с помощью app.py - всё супер
+if __name__ == "__main__":
+        # Сам запуск приложения в режиме отладки (разработки)
+        app.run(debug=True)
+#==========
+`;
+
+const blockHomeCode = `<!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -9,16 +36,24 @@ const code = `
     <title>Главная страница</title>
   </head>
   <body>
-    <!-- Мы указываем руками, что у нас есть страница /hello (example.com/hello) -->
-    <!-- Если ссылка изменится, то мы получим ошибку (example.com/helloworld) -->
+    <!-- Мы указываем руками, что у нас есть страница /hello -->
+    <!-- (example.com/hello) -->
+    <!-- Если ссылка изменится, то мы получим ошибку -->
+    <!-- (example.com/helloworld) -->
     <a href="{{ url_for('hello') }}">Ссылка</a>
 
     <!-- Flask автоматически генерирует ссылку из функции "hello" -->
     <a href="{{ url_for('hello') }}">Ссылка</a>
 
     <!-- Условия  -->
-    <!-- if - если - абсолютно всегда один и начинает блок с условиями -->
-    <!-- elif - если - может быть сколько угодно, всегда идёт после if (если есть) и продолжает блок с условиями -->
+    <!-- if - если -->
+    <!-- Абсолютно всегда один -->
+    <!-- Начинает блок с условиями -->
+
+
+    <!-- elif - если
+    <!-- Может быть в любом количестве -->
+    <!-- Всегда идёт после if (если есть) и продолжает блок с условиями -->
     <!-- else - иначе - всегда один, всегда идёт в конце после всех остальных условий (если есть) -->
     
     {% if hp == 100 %}
@@ -37,4 +72,6 @@ const code = `
 </html>
 
 `;
-el.textContent = code;
+
+elAppCode.textContent = blockAppCode;
+elHomeCode.textContent = blockHomeCode;
